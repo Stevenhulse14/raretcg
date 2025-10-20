a4d42256f57a31159cdc7b42b8ba37dcd5500804
Jonathan Ma <jonathanma388@gmail.com>
2025-10-20 10:48:59 -0400

    Update

--- backend/api/routes/cards/index.js
+++ backend/api/routes/cards/index.js
@@ -1,9 +1,30 @@
+import database from "../../database/pokemonCardData";
 const express = require("express");
 
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Cards" });
+  res.json({ message: database });
+});
+
+router.get("/:id", (req, res) => {
+  const cardId = req.params.id;
+  res.json({ message: `${cardId}` });
+});
+
+router.post("/:id", (req, res) => {
+  const cardId = req.params.id;
+  res.json({ message: `${cardId}` });
+});
+
+router.put("/:id", (req, res) => {
+  const cardId = req.params.id;
+  res.json({ message: `${cardId}` });
+});
+
+router.delete("/:id", (req, res) => {
+  const cardId = req.params.id;
+  res.json({ message: `${cardId}` });
 });
 
 module.exports = router;
