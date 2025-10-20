c4966232dc0741d3f9ccffd04cbfdfd51deaee0e
cbarwicki <cbarwicki14@gmail.com>
2025-10-20 09:46:57 -0400

    routes

--- backend/api/routes/cart/index.js
+++ backend/api/routes/cart/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.json({ message: "Admin Dashboard" });
+  res.json({ message: "Cart" });
 });
 
 module.exports = router;
--- backend/api/routes/index.js
+++ backend/api/routes/index.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const usersRouter = require("./users");
-const adminRouter = require("./admin");
-const loginRouter = require("./login");
+const authRouter = require("./auth");
+const cardsRouter = require("./cards");
+const cartRouter = require("./cart");
+const ordersRouter = require("./orders");
+const reviewsRouter = require("./reviews");
+const stripeRouter = require("./stripe");
+const discountsRouter = require("./discounts");
 
 const router = express.Router();
 
 router.use("/users", usersRouter);
-router.use("/admin", adminRouter);
-router.use("/login", loginRouter);
+router.use("/auth", authRouter);
+router.use("/cards", cardsRouter);
+router.use("/cart", cartRouter);
+router.use("/orders", ordersRouter);
+router.use("/reviews", reviewsRouter);
+router.use("/stripe", stripeRouter);
+router.use("/discounts", discountsRouter);
 
 module.exports = router;
